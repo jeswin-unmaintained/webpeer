5bc5ed0e9c10e22cc6044b30eebf9013a34c7188
jeswin <jeswinpk@agilehead.com>
2018-02-03 17:00:01 +0530

    Initial commit

--- src/webpeer.ts
+++ src/webpeer.ts
@@ -4,13 +4,15 @@ import Router = require("koa-router");
 import koaBody = require("koa-body");
 import { Server } from "http";
 import exception from "./exception";
+import * as registrationAPI from "./api/registration";
 
 async function run() {
   const app = new Koa();
   app.use(koaBody());
   
   const router = new Router();
-  router.get("/register", )
+  router.get("/register", registrationAPI.register);
+  router.get()
   
   app.use(router.routes())
 }
